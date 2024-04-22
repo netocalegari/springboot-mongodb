0e9a8b2ddeaf46ce3798a5879c86d0d5275e4589
netocalegari <netokalegari@gmail.com>
2024-04-22 11:37:00 -0300

    Refactoring to DTO pattern

--- src/main/java/com/netocalegari/springbootmongobd/resources/UserResource.java
+++ src/main/java/com/netocalegari/springbootmongobd/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.netocalegari.springbootmongobd.resources;
 
 import com.netocalegari.springbootmongobd.domain.User;
+import com.netocalegari.springbootmongobd.dto.UserDTO;
 import com.netocalegari.springbootmongobd.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/users")
@@ -21,9 +23,10 @@ public class UserResource {
     private UserService service;
 
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
+        List<UserDTO> listDTO = list.stream().map(user -> new UserDTO(user)).toList();
 
-        return ResponseEntity.ok().body(list);
+        return ResponseEntity.ok().body(listDTO);
     }
 }
