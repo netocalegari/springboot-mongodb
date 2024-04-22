a16942eec62650863a1b693fa258bce502709183
netocalegari <netokalegari@gmail.com>
2024-04-22 10:18:39 -0300

    First commit: project creation

---
+++ src/main/java/com/netocalegari/springbootmongobd/SpringbootMongobdApplication.java
@@ -0,0 +1,13 @@
+package com.netocalegari.springbootmongobd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMongobdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMongobdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/netocalegari/springbootmongobd/SpringbootMongobdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netocalegari.springbootmongobd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMongobdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
