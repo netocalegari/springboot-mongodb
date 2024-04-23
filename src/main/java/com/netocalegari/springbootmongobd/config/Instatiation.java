47a5231d0b9c84c2f3c0411c6a933386136d9e39
netocalegari <netokalegari@gmail.com>
2024-04-23 08:57:23 -0300

    Add comments on posts

--- src/main/java/com/netocalegari/springbootmongobd/config/Instatiation.java
+++ src/main/java/com/netocalegari/springbootmongobd/config/Instatiation.java
@@ -3,6 +3,7 @@ package com.netocalegari.springbootmongobd.config;
 import com.netocalegari.springbootmongobd.domain.Post;
 import com.netocalegari.springbootmongobd.domain.User;
 import com.netocalegari.springbootmongobd.dto.AuthorDTO;
+import com.netocalegari.springbootmongobd.dto.CommentDTO;
 import com.netocalegari.springbootmongobd.repository.PostRepository;
 import com.netocalegari.springbootmongobd.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,6 +37,13 @@ public class Instatiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post post2 = new Post(null, sdf.parse("21/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Aproveite", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Boa viagem mano!", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3));
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
